98593aef9b7a33fa1f96afc054bb436e7118ab69
Melissa Domgaard Account <melissa.domgaard@outlook.com>
2022-03-10 00:55:04 -0700

    Changing the navigation

---
+++ band/js/scripts.js
@@ -0,0 +1,13 @@
+function toggleMenu () {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+
+x.onclick = toggleMenu;
+
+/* var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy", treshold: 0
+    // Your custom settings go here
+} */
